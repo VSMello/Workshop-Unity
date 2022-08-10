e75778b4b227140fed70b09a5a9ef7de35580886
Vinícius Mello <vini.silveiramello13@gmail.com>
2022-08-10 17:47:41 -0300

    Walking, jumping and animations

--- Meu novo projeto/Assets/Scripts/PlayerMovement.cs
+++ Meu novo projeto/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpSpeed = 5f;
+    
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private bool isTouchingGround;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Rigidbody2D _rb;
+    private Animator _animator;
+    private float _walkInput;
+    private bool _jumpInput;
+    private float _lastXValue;
+    
+    private void Start() {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+        _animator = gameObject.GetComponent<Animator>();
+    } //end Start()
+
+    private void Update() {
+        Inputs();
+        Jump();
+    } //end Update()
+
+    private void FixedUpdate() {
+        Walk();
+    } //end FixedUpdate()
+
+    private void Inputs() {
+        _walkInput = Input.GetAxisRaw("Horizontal");
+        _jumpInput = Input.GetButtonDown("Jump");
+    } //end Inputs()
+
+    private void Walk() {
+        _rb.velocity = new Vector2(_walkInput * speed, _rb.velocity.y);
+        _lastXValue = _walkInput;
+        _animator.SetFloat("WalkX", _walkInput);
+
+        if (_rb.velocity.x != 0 && isTouchingGround) {
+            _animator.SetBool("IsWalking", true); 
+        } else {
+            _animator.SetBool("IsWalking", false);
+        }
+    } //end Walk()
+
+    private void Jump() {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (_jumpInput && isTouchingGround) {
+            _animator.SetBool("Jump", true);
+            _animator.SetBool("IsTouchingGround", false);
+            _rb.velocity = new Vector2(_rb.velocity.x, jumpSpeed);    
+        } else if (_rb.velocity.y > 0) {
+            _animator.SetBool("Jump", false);
+            _animator.SetBool("IsFalling", true);            
+        } else if (isTouchingGround) {
+            _animator.SetBool("IsTouchingGround", true);
+            _animator.SetBool("IsFalling", false);
+        }
+    } //end Jump()
 }
