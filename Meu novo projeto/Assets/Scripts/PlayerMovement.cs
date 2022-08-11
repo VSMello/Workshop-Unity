869a6376a29ff7b44c401cc1a6dbc2f68bfe7fde
Vinícius Mello <vini.silveiramello13@gmail.com>
2022-08-11 17:46:37 -0300

    Moviment and animations rework

--- Meu novo projeto/Assets/Scripts/PlayerMovement.cs
+++ Meu novo projeto/Assets/Scripts/PlayerMovement.cs
@@ -2,66 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private float jumpSpeed = 5f;
-    
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundCheckRadius;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private bool isTouchingGround;
+public class PlayerMovement : MonoBehaviour {
 
-    private Rigidbody2D _rb;
-    private Animator _animator;
-    private float _walkInput;
-    private bool _jumpInput;
-    private float _lastXValue;
-    
-    private void Start() {
-        _rb = gameObject.GetComponent<Rigidbody2D>();
-        _animator = gameObject.GetComponent<Animator>();
-    } //end Start()
+	private CharacterController2D controller;
+	private Animator animator;
 
-    private void Update() {
-        Inputs();
-        Jump();
-    } //end Update()
+	[SerializeField] private float runSpeed = 40f;
 
-    private void FixedUpdate() {
-        Walk();
-    } //end FixedUpdate()
+	private float horizontalMove = 0f;
+	private bool jump = false;
+	private bool crouch = false;
 
-    private void Inputs() {
-        _walkInput = Input.GetAxisRaw("Horizontal");
-        _jumpInput = Input.GetButtonDown("Jump");
-    } //end Inputs()
+    private void Start(){
+        controller = GetComponent<CharacterController2D>();
+        animator = GetComponent<Animator>();
+    }
 
-    private void Walk() {
-        _rb.velocity = new Vector2(_walkInput * speed, _rb.velocity.y);
-        _lastXValue = _walkInput;
-        _animator.SetFloat("WalkX", _walkInput);
+	// Update is called once per frame
+	private void Update () {
 
-        if (_rb.velocity.x != 0 && isTouchingGround) {
-            _animator.SetBool("IsWalking", true); 
-        } else {
-            _animator.SetBool("IsWalking", false);
-        }
-    } //end Walk()
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-    private void Jump() {
-        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-        if (_jumpInput && isTouchingGround) {
-            _animator.SetBool("Jump", true);
-            _animator.SetBool("IsTouchingGround", false);
-            _rb.velocity = new Vector2(_rb.velocity.x, jumpSpeed);    
-        } else if (_rb.velocity.y > 0) {
-            _animator.SetBool("Jump", false);
-            _animator.SetBool("IsFalling", true);            
-        } else if (isTouchingGround) {
-            _animator.SetBool("IsTouchingGround", true);
-            _animator.SetBool("IsFalling", false);
-        }
-    } //end Jump()
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+			animator.SetBool("IsJumping", true);
+		}
+
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		} else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+
+	}
+
+	public void OnLanding ()
+	{
+		animator.SetBool("IsJumping", false);
+	}
+
+	public void OnCrouching (bool isCrouching)
+	{
+		animator.SetBool("IsCrouching", isCrouching);
+	}
+
+	private void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
 }
