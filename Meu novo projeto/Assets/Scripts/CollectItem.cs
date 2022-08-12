53e04c642e046f5147b8f6f012e115bb74a363c4
Vinícius Mello <vini.silveiramello13@gmail.com>
2022-08-12 16:35:26 -0300

    UI gem counter

--- Meu novo projeto/Assets/Scripts/CollectItem.cs
+++ Meu novo projeto/Assets/Scripts/CollectItem.cs
@@ -1,25 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectItem : MonoBehaviour
 {
+    private int count = 0;
+    [SerializeField] private Text countText;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Start() {
+        countText.text = "0";
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.CompareTag("Player"))
-            Destroy(gameObject);
+        if (collision.CompareTag("Player")) {
+            count = int.Parse(countText.text);
+            count++;
+            countText.text = count.ToString();
+            Destroy(this.gameObject);
+        }
     }
 }
